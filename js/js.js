67c02575009b07d630e8d01d769ba187ea1bb1eb
Kovriki <polinamazitova@gmail.com>
2024-10-17 11:24:16 +0300

    css and js refactoring ( colors, comments )

--- js/js.js
+++ js/js.js
@@ -43,13 +43,13 @@ document.addEventListener('DOMContentLoaded', function () {
 // Воспроизведение звука
 
 document.addEventListener('DOMContentLoaded', function () {
-    const caseItems = document.querySelectorAll('.caseItem'); // Получаем все элементы с классом .caseItem
-    const hoverSound = document.getElementById('hoverSound'); // Звук, который будет проигрываться
+    const caseItems = document.querySelectorAll('.caseItem');
+    const hoverSound = document.getElementById('hoverSound');
 
     caseItems.forEach(item => {
         item.addEventListener('click', () => {
             if (hoverSound) {
-                hoverSound.currentTime = 0; // Перематываем звук в начало
+                hoverSound.currentTime = 0;
                 hoverSound.play().catch((error) => {
                     console.log('Ошибка воспроизведения звука:', error);
                 });
@@ -68,12 +68,10 @@ document.addEventListener('DOMContentLoaded', function () {
 
     caseItems.forEach(item => {
         item.addEventListener('click', function () {
-            // Получаем значение data-target, которое содержит ID секции
             const targetId = this.getAttribute('data-target');
             const targetElement = document.querySelector(targetId);
 
             if (targetElement) {
-                // Плавная прокрутка до нужного элемента
                 targetElement.scrollIntoView({
                     behavior: 'smooth',
                     block: 'start'
@@ -93,7 +91,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
    toggleButtons.forEach(button => {
        button.addEventListener('click', function () {
-           const resultList = this.previousElementSibling;  // Находим соответствующий список (предыдущий элемент)
+           const resultList = this.previousElementSibling;
            
            if (resultList.classList.contains('open')) {
                resultList.classList.remove('open');
@@ -126,24 +124,20 @@ function openModal() {
 document.addEventListener('DOMContentLoaded', function() {
     const page = document.querySelector('.page');
 
-    // Функция для анимации появления страницы
     function animatePageIn() {
         page.classList.add('fade-in');
 
-        // Убираем класс после завершения анимации появления (по желанию)
         setTimeout(() => {
             page.style.opacity = '1';
             page.style.transform = 'translateY(0)';
             page.classList.remove('fade-in');
-        }, 1000); // Время совпадает с длительностью transition
+        }, 1000);
     }
 
 
 
 
-    // Функция для анимации переключения страниц при клике
     function animatePageOut(href) {
-        // Запускаем анимацию исчезновения страницы
         page.classList.add('fade-out');
         console.log('fade out')
         //
@@ -152,72 +146,23 @@ document.addEventListener('DOMContentLoaded', function() {
         page.style.transform = 'translateY(60px)';
         console.log('transform')
 
-        // Переход на новую страницу после завершения анимации исчезновения
         setTimeout(() => {
  
 
             window.location.href = href;
-        }, 1000); // Время совпадает с длительностью transition
+        }, 1000); 
     }
 
-    // Инициализация анимации появления при загрузке страницы
     animatePageIn();
 
-    // Обработка кликов для перехода между страницами
     const links = document.querySelectorAll('.transition-link');
 
     links.forEach(link => {
         link.addEventListener('click', function(event) {
-            event.preventDefault(); // Отключаем стандартное поведение ссылки
+            event.preventDefault();
             const href = link.getAttribute('href');
             
-            // Инициализация анимации переключения страниц
             animatePageOut(href);
         });
     });
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-// Анимация переключения страницы
-
-
-// document.addEventListener('DOMContentLoaded', function() {
-// const links = document.querySelectorAll('.transition-link');
-
-// links.forEach(link => {
-//   link.addEventListener('click', function(event) {
-//       event.preventDefault(); // Отключаем стандартное поведение ссылки
-//       const href = link.getAttribute('href');
-      
-//       // Запускаем анимацию исчезновения страницы
-//       document.querySelector('.page').classList.add('fade-out');
-      
-//       // Ждем окончания анимации (1 секунда) и выполняем переход
-//       setTimeout(() => {
-//           window.location.href = href;
-//       }, 1000); // Время совпадает с длительностью transition
-//   });
-// });
-// });
-
-
-
-
-// анимация появления страницы
-
-// window.addEventListener('load', function() {
-//     document.page-container.classList.add('loaded'); // После загрузки страницы она плавно появляется
-// });
-
-
